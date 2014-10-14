0466e2b2309f78ba231aca248be60e21763336c4
Stoyan <stoyaneft@gmail.com>
2014-10-14 18:53:06 +0300

    Solution to pizza problem

--- week0/3.Python-file-system-problems/pizza.py
+++ week0/3.Python-file-system-problems/pizza.py
@@ -1,48 +1,124 @@
 from collections import defaultdict
 from time import time
 from datetime import datetime
+from glob import glob
+import os
+import re
 
-current_bill = defaultdict(int)
-order_files = {}
+
+current_order = defaultdict(int)
+order_filenames = []
+list_called = False
+is_saved = True
+discard_enabled = False
+finish_prompted = False
+is_finished = False
 
 
 def pizza():
     full_command = input('Enter command>')
     command = full_command.split(' ')[0]
-    COMMANDS = {'take': take, 'status': status,
-                'save': save, 'list': list_orders}
-    COMMANDS[command](full_command, current_bill)
-    pizza()
+    COMMANDS = {'take': take, 'status': status, 'save': save,
+                'list': list_orders, 'load': load, 'finish': finish}
+    if command in COMMANDS:
+        COMMANDS[command](full_command)
+    else:
+        print('Unknown command!\n' \
+              'Try one of the following:\n'
+              'take <name> <price>\n'
+              'status\n'
+              'save\n'
+              'list\n'
+              'load <number>\n'
+              'finish')
+    global is_finished
+    if not is_finished:
+        pizza()
 
 
-def take(command, current_bill):
+def take(command):
     command = command.split(' ')
     client_name = command[1]
     price = float(command[2])
-    current_bill[client_name] += price
+    current_order[client_name] += price
+    global is_saved
+    is_saved = False
     print('Taking order from {0} for {1:.2f}'.format(client_name, price))
+    global list_called
+    list_called = False
 
 
-def status(command, current_bill):
-    for client in current_bill:
-        print(client + ' - ' + str(current_bill[client]))
+def status(command):
+    for client in current_order:
+        print(client + ' - ' + str(current_order[client]))
+    global list_called
+    list_called = False
 
 
-def save(command, current_bill):
+def save(command):
     ts = time()
     filename = 'orders_' + \
         datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
-    bill_file = open(filename + '.txt', 'a')
-    for client in current_bill:
-        bill_file.write(client + ' - ' + str(current_bill[client]) + '\n')
+    order_file = open(filename + '.txt', 'a')
+    for client in current_order:
+        order_file.write(client + ' - ' + str(current_order[client]) + '\n')
     print('Saved the current order to ' + filename)
-    order_files[filename] = bill_file
-    bill_file.close()
+    order_file.close()
+    global is_saved
+    is_saved = True
+    global list_called
+    list_called = False
 
 
-def list_orders(command, current_bill):
-    for i, filename in enumerate(order_files):
+def list_orders(command):
+    full_order_filenames = glob('*.txt')
+    global order_filenames
+    order_filenames = [os.path.splitext(filename)[0] for filename in full_order_filenames]
+    for i, filename in enumerate(order_filenames):
         print('[' + str(i + 1) + '] - ' + filename)
+    global list_called
+    list_called = True
+
+
+def load(command):
+    global discard_enabled
+    if not list_called:
+        print('Use list command before loading.')
+        return
+    if not is_saved and not discard_enabled:
+        print('You have not saved the current order.\n' \
+              'If you wish to discard it, type load <number> again.')
+        discard_enabled = True
+        return
+    command = command.split(' ')
+    load_file_num = int(command[1])
+    print('Loading ' + order_filenames[load_file_num - 1])
+    load_file = open(order_filenames[load_file_num - 1] + '.txt', 'r')
+    lines = load_file.readlines()
+    global current_order
+    current_order = defaultdict(int)
+    for line in lines:
+        client_order = re.findall('[\w.]+', line)
+        current_order[client_order[0]] = float(client_order[1])
+    load_file.close()
+    discard_enabled = False
+    global list_called
+    list_called = False
+
+
+def finish(command):
+    global finish_prompted
+    if not is_saved and not finish_prompted:
+        print('You have not saved your order.\n' \
+            'If you wish to continue, type finish again.\n' \
+            'If you want to save your order, type save')
+        finish_prompted = True
+        return
+    print('Finishing order. Goodbye!')
+    global is_finished
+    is_finished = True
+    global list_called
+    list_called = False
 
 
 def main():
